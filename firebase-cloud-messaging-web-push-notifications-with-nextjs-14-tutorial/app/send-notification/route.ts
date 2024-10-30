2c87e3f6d9909ac8ef17a10f7509b0fd05aa9431
shashwat <shashwattiwari609@gmail.com>
2024-10-30 09:49:29 +0530

    new changes

--- firebase-cloud-messaging-web-push-notifications-with-nextjs-14-tutorial/app/send-notification/route.ts
+++ firebase-cloud-messaging-web-push-notifications-with-nextjs-14-tutorial/app/send-notification/route.ts
@@ -6,16 +6,16 @@ import { NextRequest, NextResponse } from "next/server";
 if (!admin.apps.length) {
   const serviceAccount = {
     type: process.env.GOOGLE_TYPE,
-    project_id: process.env.GOOGLE_PROJECT_ID,
+    project_id:process.env.GOOGLE_PROJECT_ID,
     private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    client_id: process.env.GOOGLE_CLIENT_ID,
-    auth_uri: process.env.GOOGLE_AUTH_URI,
-    token_uri: process.env.GOOGLE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
-    client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
-    universe_domain: process.env.GOOGLE_UNIVERSAL_DOMAIN
+    private_key: process.env.GOOGLE_PRIVATE_KEY!,
+    client_email:process.env.GOOGLE_CLIENT_EMAIL,
+    client_id:process.env.GOOGLE_CLIENT_ID,
+    auth_uri:process.env.GOOGLE_AUTH_URI,
+    token_uri:process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url:process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url:process.env.GOOGLE_CLIENT_CERT_URL,
+    universe_domain:process.env.GOOGLE_UNIVERSAL_DOMAIN
   };
   
   admin.initializeApp({
